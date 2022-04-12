6b2fd30ce9c3dcd9135761cb510a5bac977b8240
MAGHC <89845540+MAGHC@users.noreply.github.cog>
2022-04-12 20:11:27 +0900

    modify:수정사항 반영

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,131 @@
+import FooterTop from './FooterTop';
+import styled from 'styled-components';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterDiv>
+      <Container>
+        <GlobalUl>
+          {FOOTER_DATA.map(list => {
+            const { id, footerTitle, footerInfo } = list;
+            return (
+              <FooterTop
+                id={id}
+                key={id}
+                footerTitle={footerTitle}
+                footerInfo={footerInfo}
+              />
+            );
+          })}
+          <li>
+            <FooterPtag>TALING CENTER</FooterPtag>
+            <FooterBboing>😊 뿌잉에게 문의하기</FooterBboing>
+            <li>
+              <FooterMarginTopPtag>운영시간</FooterMarginTopPtag>
+              <FooterMarginTopPtag>
+                평일 10:00 ~ 17:00 점심 13:00 ~14:00
+              </FooterMarginTopPtag>
+            </li>
+            <SnsImg alt="sns" src="images\free-icon-network-7185719.png" />
+            <SnsImg alt="sns" src="images\free-icon-youtube-1384012.png" />
+            <SnsImg alt="sns" src="images\premium-icon-facebook-665209.png" />
+            <SnsImg alt="sns" src="images\premium-icon-instagram-665211.png" />
+          </li>
+        </GlobalUl>
+        <FooterBottom>
+          <FooterInfo>
+            상호 : 뿌잉 | 주소 : 서울시 강남구 테헤란로 427 We Work tower 427
+            Ganamgu, Seoul, Korea
+          </FooterInfo>
+          <FooterInfo>
+            대표자명 : 김효정, 임재혁, 구본희, 이강호, 류미류, 김정수
+          </FooterInfo>
+        </FooterBottom>
+      </Container>
+    </FooterDiv>
+  );
 };
 
 export default Footer;
+
+const FooterDiv = styled.div`
+  margin-top: 30px;
+  padding: 40px 0 135px;
+  border-top: 1px solid #ccc;
+`;
+
+const Container = styled.div`
+  width: 1040px;
+  margin: 0 auto;
+`;
+
+const GlobalUl = styled.ul`
+  display: flex;
+`;
+
+const FooterBottom = styled.div`
+  color: ${({ theme }) => theme.black};
+`;
+
+const FooterBboing = styled.li`
+  color: ${({ theme }) => theme.red};
+`;
+
+const FooterPtag = styled.p`
+  padding: 0 110px 20px 0;
+  font-weight: 500;
+  font-size: 16px;
+  font-weight: ${({ theme }) => theme.weightBold};
+`;
+
+const FooterMarginTopPtag = styled.p`
+  margin-top: 10px;
+`;
+
+const FooterInfo = styled.p`
+  margin-top: 10px;
+  font-size: ${({ theme }) => theme.fontMicro};
+`;
+
+const SnsImg = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-top: 35px;
+  margin-left: 10px;
+`;
+
+const FOOTER_DATA = [
+  {
+    id: 1,
+    footerTitle: 'COMPANY',
+    footerInfo: [
+      { id: 1, data: '회사소개' },
+      { id: 2, data: '블로그' },
+      { id: 3, data: '언론보도' },
+    ],
+  },
+  {
+    id: 2,
+    footerTitle: 'POLICIES',
+    footerInfo: [
+      { id: 1, data: '이용약관' },
+      { id: 2, data: '개인정보처리방침' },
+    ],
+  },
+  {
+    id: 3,
+    footerTitle: 'SUPPORT',
+    footerInfo: [
+      { id: 1, data: 'FAQ' },
+      { id: 2, data: '뿌잉센터' },
+    ],
+  },
+  {
+    id: 4,
+    footerTitle: 'B2B',
+    footerInfo: [
+      { id: 1, data: '기업교육' },
+      { id: 2, data: '브랜드제휴' },
+    ],
+  },
+];
