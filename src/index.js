5e21c213ed36ef5d309ea25aee69baf7fb69dc38
bonimac <bonimac@bonimacui-MacBookPro.local>
2022-04-12 12:04:51 +0900

    Modify : 초기세팅 수정

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Router />
+  </ThemeProvider>
 );
