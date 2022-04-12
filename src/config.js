cdc467bf20474af922136aa56f7ad8a5c1d042cd
MAGHC <89845540+MAGHC@users.noreply.github.cog>
2022-04-12 16:42:17 +0900

    ADD:레이아웃 추가 및 백엔드 통신완료

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
-export const BASE_URL = 'http://52.78.104.136:8000';
+export const BASE_URL = 'http://10.58.7.101:8000';
 export const API = {
   login: `${BASE_URL}/users/signin`,
-  join: `${BASE_URL}/users/signup`,
+  join: `${BASE_URL}/users/kakao-login`,
   product: `${BASE_URL}/product`,
   products: `${BASE_URL}/products`,
   carts: `${BASE_URL}/carts`,
 };
+
+export default API;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,96 @@
+import { useNavigate } from 'react-router-dom';
+import API from '../../config';
+import styled from 'styled-components';
+
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  function kakaLogin() {
+    window.Kakao.init(process.env.REACT_APP_KAKAO);
+    window.Kakao.Auth.login({
+      scope: 'profile_nickname, profile_image',
+      success: function (res) {
+        fetch(`${API.join}`, {
+          method: 'POST',
+          headers: {
+            Authorization: res.access_token,
+          },
+        })
+          .then(res => res.json())
+          .then(res => {
+            const restoken = res.access_token.access_token;
+            const profile = res.access_token.profile_image;
+            const nickname = res.access_token.kakao_nickname;
+            localStorage.setItem('token', restoken);
+            localStorage.setItem('profile', profile);
+            localStorage.setItem('nickname', nickname);
+            goToMain();
+          });
+      },
+    });
+  }
+
+  const goToMain = () => {
+    alert('로그인에 성공하였습니다.');
+    navigate('/');
+  };
+
+  return (
+    <LoginSection>
+      <LoginBox>
+        <MainImg
+          alt="로그인배경이미지"
+          src="/images/login background img.jpg"
+        />
+        <PromotionalText>가입 즉시 12만원 쿠폰팩을 드려요!</PromotionalText>
+        <KaKaoBtn type="button" onClick={kakaLogin}>
+          <KakaoLoginImg
+            alt="카카오로그인이미지"
+            src="/images/kakao_login_medium_wide.png"
+          />
+        </KaKaoBtn>
+      </LoginBox>
+    </LoginSection>
+  );
 };
 
 export default Login;
+
+const LoginSection = styled.section`
+  min-height: calc(100vh - 500px);
+  padding: 130px 0 200px;
+  text-align: center;
+  ${props => props.theme.wrapper};
+`;
+
+const LoginBox = styled.div`
+  width: 420px;
+  margin: 0 auto;
+  padding: 30px 20px;
+  border: 1px solid #eee;
+`;
+
+const MainImg = styled.img`
+  width: 70%;
+  max-width: 100%;
+  max-height: 100%;
+  vertical-align: top;
+`;
+
+const PromotionalText = styled.h2`
+  margin: 20px 0;
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 30px;
+`;
+
+const KaKaoBtn = styled.button`
+  border: none;
+  background-color: white;
+`;
+
+const KakaoLoginImg = styled.img`
+  margin: 5px 0;
+  &:hover {
+    cursor: pointer;
+  }
+`;
