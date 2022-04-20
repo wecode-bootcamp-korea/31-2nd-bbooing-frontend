b587b94f308044b0d1327b453788e454ddcfe7c9
Jaehyuksssss <ljh9510@naver.com>
2022-04-20 09:13:14 +0900

    Modify : review 수정

--- src/pages/WishList/WishList.js
+++ src/pages/WishList/WishList.js
@@ -1,5 +1,63 @@
-const WishList = () => {
-  return <div>WishList</div>;
-};
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import WishContents from './WishComponent/WishContents';
+import API from '../../config';
 
+const WishList = () => {
+  const [lectures, setLectures] = useState([]);
+
+  const getWishListData = () => {
+    fetch(`${API.carts}`)
+      .then(res => res.json())
+      .then(res => {
+        setLectures(res.results);
+      });
+  };
+
+  useEffect(() => {
+    getWishListData();
+  }, []);
+
+  return (
+    <Wrapper>
+      <WishContainer>
+        <TitleBox>
+          <h2>나의 뿌잉</h2>
+          <p>찜</p>
+        </TitleBox>
+
+        {lectures &&
+          lectures.map(lecture => (
+            <WishContents
+              lecture={lecture}
+              setLectures={setLectures}
+              key={lecture}
+            />
+          ))}
+      </WishContainer>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.div`
+  ${({ theme }) => theme.wrapper()}
+  display: flex;
+  flex-direction: column;
+`;
+const WishContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const TitleBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 1000px;
+  h2 {
+    font-size: 26px;
+    font-weight: 400;
+  }
+  color: #333;
+  line-height: 1.85;
+  cursor: pointer;
+`;
 export default WishList;
